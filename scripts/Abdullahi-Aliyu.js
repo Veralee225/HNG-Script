67fd6700ebcc98c52029707fd8311959fc1bd03c
Veralee225 <59749997+Veralee225@users.noreply.github.com>
2020-06-06 03:46:54 +0100

    scripts\Adiele-Vera.js

--- scripts/Abdullahi-Aliyu.js
+++ scripts/Abdullahi-Aliyu.js
@@ -1,7 +1,9 @@
+
 let me = {
-	Name: "Abdullahi Aliyu",
-	HNGID: "HNG-01827",
-	email: "abdoulfurya@gmail.com",
-	language: "JavaScript"
+        Name: "Abdullahi Aliyu",
+        HNGID:  "HNG-01827",
+        email: "abdoulfurya@gmail.com",
+        language: "JavaScript"
 }
-console.log(`Hello World, this is ${me.Name} with HNGi7 ID ${me.HNGID} and email ${me.email} using ${me.language} for stage 2 task`);
+console.log(`Hello World, this is ${me.Name} with HNGi7 ID ${me.HNGID} using ${me.language} for stage 2 task. ${me.email}`);
+
